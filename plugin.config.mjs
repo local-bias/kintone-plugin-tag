009d9f2f7e6c7e5b516779236c76c1ee94ac9c04
Ribbit <echo07200503@gmail.com>
2023-06-08 09:29:45 +0900

    🎉 1.3.0

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -8,7 +8,7 @@ export default {
   manifest: {
     base: {
       manifest_version: 1,
-      version: '1.2.1',
+      version: '1.3.0',
       type: 'APP',
       name: {
         en: 'Tagging Plugin',
