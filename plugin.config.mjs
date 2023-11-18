d343a13fce830c037f21104960fd69c48eced5b1
Ribbit <echo07200503@gmail.com>
2023-11-18 18:30:58 +0900

    🚚 Update file paths in plugin.config.mjs

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -43,9 +43,9 @@ export default {
       config: { js: [`${localhost}/dist/dev/config/index.js`] },
     },
     prod: {
-      desktop: { js: [`${cdn}/cdn/desktop.js`] },
-      mobile: { js: [`${cdn}/cdn/desktop.js`] },
-      config: { js: [`${cdn}/cdn/config.js`] },
+      desktop: { js: ['desktop.js'] },
+      mobile: { js: ['desktop.js'] },
+      config: { js: ['config.js'] },
     },
     standalone: {
       desktop: { js: ['desktop.js'] },
